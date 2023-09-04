98bf5ce878bdbaf71216208d869496ff5710f539
electroae4 <electro.ae4@gmail.com>
2023-09-04 16:10:17 +0200

    .\7 kyu\valid-parentheses.js

---
+++ 7 kyu/valid-parentheses .js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+//   Write a function that takes a string of parentheses, and determines if the order of the
+//   parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+//
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+
+
+// SOLUTION:
+function validParentheses(parenStr) {
+  let str = parenStr;
+  while (str.includes("()")) str = str.replaceAll("()", "");
+  return str === "" ? true : false;
+}
+
+
